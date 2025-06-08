bbe96d0c610b388fe2da34b9f7d5902170418178
Tmaneesh19 <maneeshkumar19022006@gmail.com>
2025-06-08 11:09:35 +0530

    hs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        hello world
-      </main>
+    <Dashboard />
   );
 }
